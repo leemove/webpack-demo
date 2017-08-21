48d65b77a0c75ca148d6efaa7e4756ddbcda7f3b
leemove <923522454@qq.com>
2017-08-21 18:21:23 +0800

    静态资源处理

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,22 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 99999999999
+        }
+      }]
+    }]
   }
 }
